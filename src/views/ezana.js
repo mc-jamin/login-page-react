847c185931f17f7235e9b8589c8369e09f6fd887
Jamin <mc.jamin007@gmail.com>
2022-05-12 17:17:04 +0300

    improved ui with functionalities

--- src/views/ezana.js
+++ src/views/ezana.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Helmet } from 'react-helmet'
 import './ezana.css'
 
 
 const Ezana = (props) => {
+
+  const [details, setDetails] = useState(
+    {
+      username:'',
+      password: ''
+    })
+    const [err, setErr] = useState({})
+    const validateDetails = (details) =>{
+      let err = {}
+      if (details.username===''){
+        err=Object.assign(err, {
+          username: 'This field cannot be empty'
+        })
+      }
+      if(details.password===''){
+        err=Object.assign(err, {
+          password: 'This field cannot be empty'
+        })
+      }
+      return err
+    }
+    const changedInput =(evt)=>{
+      evt.persist()
+      setDetails( details => ({...details, [evt.target.name]: evt.target.value})
+      )
+    }
   return (
     <><Helmet>
       <title>Login Here</title>
@@ -16,11 +42,20 @@ const Ezana = (props) => {
           </div>
           <div className="ezana-email-input">
             <label className="ezana-text06">Email</label> <br></br>
-            <input id='email-input' type='email' placeholder='Enter your email' />
+            <input name ='username' value={details.username} id='email-input' type='email' placeholder='Enter your email' 
+            onChange={changedInput.bind(this)}/>
+            {
+              err.hasOwnProperty('username') && <p className='error-message'>{err.username}</p>              
+            }
           </div>
           <div className="ezana-password-input">
             <label className="ezana-text06">Password</label> <br></br>
-            <input id='password-input' type='email' placeholder='Enter passsword' />
+            <input name='password'value={details.password} id='password-input' type='password' placeholder='Enter passsword' 
+            onChange={changedInput.bind(this)}/>
+            {
+              err.hasOwnProperty('password') && <p className='error-message'>{err.password}</p>
+
+            }
           </div>
           <div className="ezana-remember-forget">
             <span className="ezana-text11">Forgot password?</span>
@@ -28,7 +63,11 @@ const Ezana = (props) => {
             <input type='checkbox' className="ezana-image5" />
           </div>
           <div>
-            <input type='button' value='Sign in' className="ezana-sign-in-button" />
+            <input type='button' value='Sign in' className="ezana-sign-in-button" 
+            onClick={ evt =>{
+              evt.preventDefault()
+              setErr({...validateDetails(details)})
+            }} />
           </div>
           <div className="ezana-text">
                 <span className="ezana-text01">Don’t have an account?</span>
